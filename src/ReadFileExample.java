6baab4d655b5c51b0fa126901e9042aa365c5ce8
phuong23401 <hoaphuong23401@gmail.com>
2021-04-26 17:26:53 +0700

    add

---
+++ src/ReadFileExample.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class ReadFileExample {
+    public void readFileText(String filePath) {
+        try {
+            File file = new File(filePath);
+
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = "";
+            int sum = 0;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                sum += Integer.parseInt(line);
+            }
+            reader.close();
+
+            System.out.println("Tổng = " + sum);
+        } catch (Exception e) {
+            System.err.println("Fie không tồn tại or nội dung có lỗi!");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Nhập đường dẫn file: ");
+        Scanner scanner = new Scanner(System.in);
+        String path = scanner.nextLine();
+
+        ReadFileExample readfileEx = new ReadFileExample();
+        readfileEx.readFileText(path);
+    }
+}
